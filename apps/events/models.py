1c3cfc1f10e08daa4fb79bfc8c9c2934f3c43b1c
Bekbolsun <bekbolsuntk@gmail.com>
2023-12-14 20:20:12 +0600

    Beka

--- apps/events/models.py
+++ apps/events/models.py
@@ -10,22 +10,20 @@ class Category(models.Model):
         return f"{self.name}"
 
 
-class EventDay(models.Model):
-    start_date = models.DateTimeField()
-    end_date = models.DateTimeField()
-
-
 class Event(models.Model):
     title = models.CharField(max_length=150)
     description = models.TextField()
     banner = models.ImageField(upload_to='media/', null=True, blank=True)
     language = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=5, decimal_places=2)
-    # guest = models.ManyToManyField(User, on_delete=models.CASCADE)
     category = models.ManyToManyField(Category)
-    event_dates = models.ManyToManyField(EventDay)
-    # address
 
     def __str__(self):
         return f'{self.title} '
 
+
+class EventDay(models.Model):
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    # week_day = models.CharField(max_length=50, choices=)
