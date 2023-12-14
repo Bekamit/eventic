f405c25a3c5f6365229d9847f2df870315a73009
Bekbolsun <bekbolsuntk@gmail.com>
2023-12-14 20:28:09 +0600

    beka

--- apps/events/views.py
+++ apps/events/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from .models import Event
 from .serializers import EventSerializer
 
-import django_filters
+# import django_filters
 
 
 class EventRetrieveAPIView(generics.RetrieveAPIView):
