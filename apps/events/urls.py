8ec9fd4c829913d79d92f323166e70c9e5fc270a
Bekbolsun <bekbolsuntk@gmail.com>
2023-12-14 20:55:28 +0600

    Beka33

--- apps/events/urls.py
+++ apps/events/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from .views import EventRetrieveAPIView, EventListApiView
+from .views import EventRetrieveAPIView
 
 urlpatterns = [
-    path("", EventListApiView.as_view()),
+    # path("", EventListApiView.as_view()),
     path("event/<int:pk>/", EventRetrieveAPIView.as_view()),
 ]
